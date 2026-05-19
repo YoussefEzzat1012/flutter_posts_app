e0a6da9c600dcf7dbdc96c9bdb9d4131029b7e38
youssef <youssefezzat937@gmail.com>
2026-05-19 08:12:55 +0300

    Add string and context extensions

---
+++ lib/core/utils/extensions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+extension BuildContextExtension on BuildContext {
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError ? Colors.red : Colors.green,
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      ),
+    );
+  }
+}
